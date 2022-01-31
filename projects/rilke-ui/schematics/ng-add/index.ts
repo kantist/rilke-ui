0f27f88afec60a1706ad49201d94ac1c318faec8
Emirhan Yumak <emiryumak@gmail.com>
2022-01-31 12:20:06 +0300

    Update index.ts

--- projects/rilke-ui/schematics/ng-add/index.ts
+++ projects/rilke-ui/schematics/ng-add/index.ts
@@ -33,7 +33,7 @@ function addStyleToWorkspaceFile(workspace: workspaces.WorkspaceDefinition): Rul
 		let configPath = './angular.json';
 
 		if (host.exists(configPath)) {
-			let currentAngularJson = host.read(configPath).toString('utf-8');
+			let currentAngularJson = host.read(configPath)!.toString('utf-8');
 			let json = JSON.parse(currentAngularJson);
 			let optionsJson = json['projects'][projectName]['architect']['build']['options'];
 
