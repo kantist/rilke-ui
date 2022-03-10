a64d63a8e83a3e966420fc2995493ac503011e9c
Emirhan Yumak <emiryumak@gmail.com>
2022-03-10 18:45:21 +0300

    Bottom Sheet Event

--- projects/rilke-ui/src/lib/services/bottom-sheet.service.ts
+++ projects/rilke-ui/src/lib/services/bottom-sheet.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
 import {
 	MatBottomSheet,
 	MatBottomSheetConfig,
@@ -9,19 +9,27 @@ import { IBottomSheetOptions } from '../interfaces/bottom-sheet';
 
 @Injectable({ providedIn: 'root' })
 export class BottomSheetService {
+	status: EventEmitter<boolean>;
+
 	constructor(private _matBottom: MatBottomSheet) {}
 
 	open(config: IBottomSheetOptions) {
+		this.status.emit(true);
+
 		this._matBottom.open(config.component, config);
 	}
 
 	close() {
+		this.status.emit(false);
 		this._matBottom.dismiss();
 	}
 
 	afterClose() {
 		return this._matBottom._openedBottomSheetRef
 			.afterDismissed()
-			.pipe(tap((res: any) => res));
+			.pipe(tap((res: any) => {
+				this.status.emit(false);
+				return res
+			}));
 	}
 }
